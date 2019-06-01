12bcf2cb8abb4de2d1c4a00edf3a1e2799e2444f
CodeXRook <lacekitfly@gmail.com>
2019-06-01 18:43:09 -0400

    now we have admin access

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase admin');
 
-
-
+//Now we have access to admin object
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
