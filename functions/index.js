3e1de77a6ff963b20ca4b0611b40afb31cd869a6
CodeXRook <lacekitfly@gmail.com>
2019-06-01 18:41:56 -0400

    add admin

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase admin');
+
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
