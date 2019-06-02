07cfa8498deb10a967b0f9ab990d60c24e7145a5
CodeXRook <lacekitfly@gmail.com>
2019-06-01 21:30:31 -0400

    updated error catch

--- functions/index.js
+++ functions/index.js
@@ -38,6 +38,7 @@ exports.createScream = functions.https.onRequest((req, res) => {
         res.json({message: `document ${doc.id} created successfully`});
     })
     .catch(err => {
-        res.status(500).json()
-    })
+        res.status(500).json({error: 'something went wrong'});
+        console.error(err);
+    });
 });
